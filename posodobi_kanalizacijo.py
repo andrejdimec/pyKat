8555c34fe7c66940f125beae953036b1498e20ab
andrejdimec <dimec.andrej@gmail.com>
2024-09-13 10:54:13 +0200

    sreda 10.9.

--- posodobi_kanalizacijo.py
+++ posodobi_kanalizacijo.py
@@ -3,11 +3,15 @@ import sys
 import arcpy
 from arcgis.gis import GIS
 import os
-
+import xml.dom.minidom as DOM
 from PySide6 import QtCore
+from sympy import false
+
 import vars
 from PySide6.QtWidgets import QDialog
 from PySide6.QtCore import Slot, Qt
+
+# from test9 import mymap
 from ui_frm_posodobi_kanalizacijo import Ui_frmPosodobiKan
 from comm import Comm
 from logger import Logger
@@ -92,19 +96,26 @@ class PosodobiKanalizacijo(QDialog):
 
         # Prijavi se na AGOL
         self.presledek()
-        self.log("Prijavljam se v ArcGIS Online...")
+        self.log("Prijava v ArcGIS Online...")
         try:
             arcpy.SignInToPortal(portal_url, username, password)
             gis = GIS(portal_url, username, password)
-            self.logc("Prijava uspešna", green)
+            self.logc("Uspešno.", green)
 
         except Exception as e:
             self.logc("Napaka pri prijavi " + str(e), err)
             dalje = False
 
         if dalje:
+            project = os.path.join(project_path, aprx_name)
+            aprx = arcpy.mp.ArcGISProject(project)
+            mymap = aprx.listMaps(map_name)[0]
+
             self.presledek()
             for layer_pro in layer_ok:
+                layer = mymap.listLayers(layer_pro)[0]
+                output_service_name = layer_pro
+                # self.log("layer" + str(layer.name))
                 self.logc("Posodabljam layer " + layer_pro, black)
                 search_result = gis.content.search(
                     query=layer_pro, item_type="Feature Layer"
@@ -114,8 +125,126 @@ class PosodobiKanalizacijo(QDialog):
                 else:
                     self.logc(f"Layer še ne obstaja -> dodajam.", red)
 
+                # Dejansko posodobi
+
+                # preveri in naredi scratch folder
+                scratch_folder = arcpy.env.scratchFolder
+                if not scratch_folder:
+                    scratch_folder = os.path.join(os.getcwd(), "scratch")
+                    os.makedirs(scratch_folder, exist_ok=True)
+                    arcpy.env.scratchFolder = scratch_folder
+
+                # self.log(f"Scratch folder: {scratch_folder}")
+
+                # Define paths for service definition draft and staged service
+                sddraft_file = os.path.join(scratch_folder, f"{layer_pro}.sddraft")
+                sd = os.path.join(scratch_folder, f"{layer_pro}.sd")
+                thumb = os.path.join(scratch_folder, "Thumbnail.png")
+
+                novi = True
+
+                if novi:
+                    self.log("Novi način sddraft")
+                    # sharing draft - novi način
+                    # Create FeatureSharingDraft and set metadata, portal folder, export data properties, and CIM symbols
+                    server_type = "HOSTING_SERVER"
+                    sddraft = mymap.getWebLayerSharingDraft(
+                        server_type, "FEATURE", output_service_name
+                    )
+                    # sddraft = mymap.getWebLayerSharingDraft(
+                    #     server_type, "FEATURE", output_service_name
+                    # )
+                    sddraft.credits = "These are credits"
+                    sddraft.description = "This is description"
+                    sddraft.summary = "This is summary"
+                    sddraft.tags = "tag1, tag2"
+                    sddraft.useLimitations = "These are use limitations"
+                    sddraft.portalFolder = ""
+                    sddraft.allowExporting = True
+                    sddraft.useCIMSymbols = True
+
+                    # Create Service Definition Draft file
+                    sddraft.exportToSDDraft(sddraft_file)
+
+                    # Read the .sddraft file
+                    docs = DOM.parse(sddraft_file)
+                    key_list = docs.getElementsByTagName("Key")
+                    value_list = docs.getElementsByTagName("Value")
+
+                    # Change following to "true" to share
+                    SharetoOrganization = "false"
+                    SharetoEveryone = "true"
+                    SharetoGroup = "false"
+                    # If SharetoGroup is set to "true", uncomment line below and provide group IDs
+                    GroupID = ""  # GroupID = "f07fab920d71339cb7b1291e3059b7a8, e0fb8fff410b1d7bae1992700567f54a"
+
+                    # Each key has a corresponding value. In all the cases, value of key_list[i] is value_list[i].
+                    for i in range(key_list.length):
+                        if key_list[i].firstChild.nodeValue == "PackageUnderMyOrg":
+                            value_list[i].firstChild.nodeValue = SharetoOrganization
+                        if key_list[i].firstChild.nodeValue == "PackageIsPublic":
+                            value_list[i].firstChild.nodeValue = SharetoEveryone
+                        if key_list[i].firstChild.nodeValue == "PackageShareGroups":
+                            value_list[i].firstChild.nodeValue = SharetoGroup
+                        if (
+                            SharetoGroup == "true"
+                            and key_list[i].firstChild.nodeValue == "PackageGroupIDs"
+                        ):
+                            value_list[i].firstChild.nodeValue = GroupID
+
+                    # Write to the .sddraft file
+                    f = open(sddraft_file, "w")
+                    docs.writexml(f)
+                    f.close()
+
+                else:
+                    # Create a service definition draft
+                    self.presledek()
+                    self.log(f"Creating service definition draft...")
+                    arcpy.mp.CreateWebLayerSDDraft(
+                        map_or_layers=layer,  # Input layer object
+                        out_sddraft=sddraft,  # Output path for .sddraft file
+                        service_name=output_service_name,  # Name of the web service to be published
+                        service_type="HOSTING_SERVER",  # ArcGIS Online hosting server
+                        server_type="MY_HOSTED_SERVICES",  # Service connection type for ArcGIS Online
+                        folder_name="Kanalizacija",  # Folder on ArcGIS Online (empty means root folder)
+                        overwrite_existing_service=True,  # Overwrite existing service with the same name
+                        summary=f"Web layer of the '{layer_pro}' layer",  # Summary for the service
+                        tags=f"arcpy, {layer_pro}, ArcGIS Online",  # Tags to describe the web layer
+                        description=f"This web layer is created from the '{layer_pro}' layer in ArcGIS Pro",
+                        # Detailed description
+                    )
+
+                self.log(f"Staging service definition...")
+                try:
+                    arcpy.StageService_server(sddraft_file, sd)
+                    warnings = arcpy.GetMessages(1)
+                    if warnings:
+                        self.log(f"Warnings:")
+                        for warning in warnings:
+                            self.log(f"\t{warning}")
+                except Exception as e:
+                    dalje = False
+                    self.logc("Staging error. - {}".format(str(e)), err)
+                    self.brisi_temp(sddraft, sd, thumb)
+                    # sys.exit("Napaka pri staging.")
+
+                # if dalje:
+                #     # Upload the staged service definition to ArcGIS Online
+                #     self.log(f"Uploading '{layer_pro}'...")
+                #     try:
+                #         arcpy.UploadServiceDefinition_server(sd, "MY_HOSTED_SERVICES")
+                #         self.logc(f"Layer '{layer_pro}' - uspešno posodobljen.", green)
+                #
+                #     except Exception as e:
+                #         self.logc("Napaka pri upload - {}".format(str(e)), err)
+                #         self.brisi_temp(sddraft, sd, thumb)
+                #         dalje = False
+                #         # sys.exit("Napaka pri upload.")
+
+                # self.brisi_temp(sddraft, sd, thumb)
         # Konec
-        self.presledek()
+        # self.presledek()
         minutes, seconds = stop(start_time)
         self.logc(f"Končano v {minutes} min {seconds} sek.", blue)
         if dalje:
@@ -160,7 +289,7 @@ class PosodobiKanalizacijo(QDialog):
         self.log(f"Preverjam layerje, če obstajajo v ArcGis Pro...")
         layer_ok.clear()
         for layer_name in layer_izbran:
-            print(": " + layer_name)
+            # print(": " + layer_name)
             odstrani_layer = ""
             layer = None
             for lyr in mymap.listLayers():
@@ -173,12 +302,9 @@ class PosodobiKanalizacijo(QDialog):
             if not layer:
                 pass
                 self.logc(f"Layer '{layer_name}' ne obstaja in ne bo posodobljen.", err)
-                # layer_izbran.remove(layer_name)
-                # print("remove: " + layer_name)
-                # sys.exit(1)
 
-        for lyr in layer_ok:
-            self.log("ok: " + lyr)
+        # for lyr in layer_ok:
+        #     self.log("ok: " + lyr)
 
     def zacetek(self):
         self.cb_jaski.setChecked(False)
@@ -209,6 +335,22 @@ class PosodobiKanalizacijo(QDialog):
 
     def presledek(self):
         self.log("\n")
+
+    def brisi_temp(self, sddraft, sd, thumb):
+        # Zbriši temp datoteke
+        try:
+            self.log("Brisanje začasnih datotek...")
+            if os.path.exists(sddraft):
+                os.remove(sddraft)
+                # self.log(f"Deleted: {sddraft}")
+            if os.path.exists(sd):
+                os.remove(sd)
+                # self.log(f"Deleted: {sd}")
+            if os.path.exists(thumb):
+                os.remove(thumb)
+                # self.log(f"Deleted: {thumb}")
+        except Exception as e:
+            self.logc(f"Napaka pri brisanju temp datotek: {str(e)}", err)
 
 
 def stop(st):
